c7ee1d5576b892cb03f9fafb864a6dfb4ef14eb4
Gibbor28 <75150041+Gibbor-SAP@users.noreply.github.com>
2023-04-20 19:11:57 -0300

    Inicio del proyecto, probando el codigo

---
+++ src/main/java/com/alura/gerenciador/servlet/HolaMundoServlet.java
@@ -0,0 +1,30 @@
+package com.alura.gerenciador.servlet;
+
+import java.awt.print.Printable;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//hola
+@WebServlet(urlPatterns = "/hola") //este comando sirve para cambiar la url de la página
+public class HolaMundoServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter(); // Objeto para imprimir los datos que queremos que la página muestre
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hola! felicitaciones por crear tu primer Servlet");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("Ejecutando Servlet sin problemas");
+		
+	}
+	
+}
